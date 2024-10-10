41fe57cf49919f398e0a7f7b2d9a33e64a4a3d49
changhao2002 <huchanghao@metabit.network>
2024-10-10 15:37:49 +0800

    add the chart

--- src/app/coin-detail/coin-detail.component.ts
+++ src/app/coin-detail/coin-detail.component.ts
@@ -1,13 +1,16 @@
-import { Component , OnInit } from '@angular/core';
+import { Component , OnInit, ViewChild } from '@angular/core';
 import { ApiService } from '../service/api.service';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ChartConfiguration , ChartType } from 'chart.js'
+import { BaseChartDirective } from 'ng2-charts'
 
 @Component({
   selector: 'app-coin-detail',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    BaseChartDirective
   ],
   templateUrl: './coin-detail.component.html',
   styleUrl: './coin-detail.component.scss'
@@ -18,6 +21,35 @@ export class CoinDetailComponent implements OnInit {
   coinId!: string;
   days: number = 1;
   currency : string = 'MYR';
+  public lineChartData: ChartConfiguration['data'] = {
+    datasets : [
+      {
+        data: [],
+        label: `Price Trends`,
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: '#009688',
+        pointBackgroundColor: '#009688',
+        pointBorderColor: '#009688',
+        pointHoverBackgroundColor: '#009688',
+        pointHoverBorderColor: '#009688',
+      }
+    ],
+    labels: []
+  };
+  public lineChartOptions: ChartConfiguration['options'] = {
+    elements: {
+      point: {
+        radius: 1
+      }
+    },
+
+    plugins: {
+      legend: { display: true },
+    }
+  };
+  public lineChartType : ChartType = 'line';
+  @ViewChild(BaseChartDirective) myLineChart !: BaseChartDirective;
+
   constructor(
     private api : ApiService,
     private activateRoute : ActivatedRoute
